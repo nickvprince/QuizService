d7012b1015a9c48cb5673bb96ee18d1eaf715aba
Babywise <islamdoesart@gmail.com>
2022-10-20 13:25:57 -0400

    Inital functionality added to Quiz Object

--- Enterprise_Solution_Quizzes/QuizSharedDocker/objects/Quiz.cpp
+++ Enterprise_Solution_Quizzes/QuizSharedDocker/objects/Quiz.cpp
@@ -1,1 +1,39 @@
 #include "Quiz.h"
+
+/// <summary>
+/// Constructor for Quiz
+/// Constructor is called whenever a Quiz is to be created
+/// </summary>
+/// <param name="title"></param>
+Quiz::Quiz(std::string title) {
+	this->title = title;
+}
+
+/// <summary>
+/// Modify the existing title of a Quiz
+/// </summary>
+/// <param name="title"></param>
+/// If modification is successful
+/// <returns>True</returns>
+/// If modification is not successful
+/// <returns>False</returns>
+bool Quiz::setTitle(std::string title)
+{
+	this->title = title;
+	if ( this->title == title ) {
+		return true;
+	}
+	return false;
+}
+
+/// <summary>
+/// Getter for title
+/// </summary>
+/// <returns>title</returns>
+std::string Quiz::getTitle() {
+	return this->title;
+}
+
+bool Quiz::deleteQuiz() {
+
+}
