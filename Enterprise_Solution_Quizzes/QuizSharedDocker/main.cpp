922eee931b05b679d25f41ceb047e70261e34f49
Danny Smith <Dsmith2304@conestogac.on.ca>
2022-10-26 01:29:23 -0400

    Javascript modified to redirect to a page savepoolPass or fail which redirects back to createPool with query string for if pool was created or not for an alert.

--- Enterprise_Solution_Quizzes/QuizSharedDocker/main.cpp
+++ Enterprise_Solution_Quizzes/QuizSharedDocker/main.cpp
@@ -33,7 +33,7 @@ void sendFile(response& res, string filename, string contentType) {
 	// Read the input file 
 	ifstream input(filepath, ifstream::in);
 
-	if ( input ) {
+	if (input) {
 
 		ostringstream contents;
 		contents << input.rdbuf();
@@ -43,7 +43,8 @@ void sendFile(response& res, string filename, string contentType) {
 		res.set_header("Content-Type", contentType);
 		res.write(contents.str());
 
-	} else {
+	}
+	else {
 
 		// Set the proper error code and write the message to the browser when there is an error
 		res.code = 404;
@@ -65,9 +66,10 @@ void sendStyle(response& res, string filename) {
 
 void sendImage(response& res, string filename) {
 
-	if ( filename.find(".jpeg") != string::npos ) {
+	if (filename.find(".jpeg") != string::npos) {
 		sendFile(res, "images/" + filename, "image/jpeg");
-	} else {
+	}
+	else {
 		sendFile(res, "images/" + filename, "image/png");
 	}
 }
@@ -88,7 +90,7 @@ int main() {
 #ifdef _WIN32
 
 	std::cout << "Hello World" << std::endl;
-	
+
 	QuestionPool q("pool1");
 	std::cout << q.addQuestion("Question 1", 1);
 	std::cout << q.addOption("Question 1", "Option 1", 1);
@@ -97,8 +99,8 @@ int main() {
 	std::cout << q.addOption("Question 2", "Option 1", 0);
 	std::cout << q.addOption("Question 2", "Option 2", 1);
 	q.save();
-	
-	
+
+
 #endif // _WIN32
 
 #ifdef __linux__
@@ -106,73 +108,109 @@ int main() {
 
 	// Default Route
 	CROW_ROUTE(app, "/")
-	([](const request& req, response& res) {
+		([](const request& req, response& res) {
 		sendHtml(res, "index.html");
-	});
+			});
 
+	CROW_ROUTE(app, "/savepool")
+		([](const request& req, response& res) {
+
+		std::string poolname = req.url_params.get("pool");
+		QuestionPool q(poolname);
+		std::vector<char*> questions = req.url_params.get_list("Questions");
+		int size = questions.size();
+		for (int i = 0; i < size; i++) {
+			q.addQuestion(questions.at(i), 1);
+			std::vector<char*> answers = req.url_params.get_list("Q" + to_string(i) + "A");
+			int sizeAnswers = answers.size();
+			for (int b = 0; b < sizeAnswers; b++) {
+
+				q.addOption(questions.at(i), answers.at(b), 0);
+			}
+		}
+
+		if (q.save() == true) {
+			sendHtml(res, "savepoolPass.html");
+		}
+		else {
+			sendHtml(res, "savepoolFail.html");
+		}
+		
+			});
+	/*
+	CROW_ROUTE(app, "/savepool")
+		([](const request& req, response& res, string filename) {
+		std::string poolname = req.url_params.get("pool");
+		QuestionPool q(poolname);
+		std::vector<char*> questions = req.url_params.get_list("Questions");
+		int size = questions.size();
+		for (int i = 0; i < size; i++) {
+			q.addQuestion(questions.at(i), 1);
+			std::vector<char*> answers = req.url_params.get_list("Q" + to_string(i) + "A");
+			int sizeAnswers = answers.size();
+			for (int b = 0; b < sizeAnswers; b++) {
+
+				q.addOption(questions.at(i), answers.at(b), 0);
+			}
+		}
+
+		q.save();
+		std::string str = "hello.html";
+
+		sendHtml(res, str);
+			});
+*/
 	// Calling html from products pages
 	CROW_ROUTE(app, "/<string>")
-	([](const request& req, response& res, string filename) {
+		([](const request& req, response& res, string filename) {
 		//createQuiz Query
-		if(filename == "createQuiz.html"){
+		if (filename == "createQuiz.html") {
 			auto quizTitle = req.url_params.get("quizTitle");
 			auto quizDuration = req.url_params.get("quizDuration");
 			auto quizPool = req.url_params.get("quizPool");
 			auto quizStartDate = req.url_params.get("quizStartDate");
 			auto quizEndDate = req.url_params.get("quizEndDate");
 
-			ostringstream quizTitleString, quizDurationString, quizPoolString, 
-						quizStartDateString, quizEndDateString;
+			ostringstream quizTitleString, quizDurationString, quizPoolString,
+				quizStartDateString, quizEndDateString;
 
-			quizTitleString << quizTitle ? quizTitle: "";
-			quizDurationString << quizDuration ? quizDuration: "";
-			quizPoolString << quizPool ? quizPool: "";
-			quizStartDateString << quizStartDate ? quizStartDate: "";
-			quizEndDateString << quizEndDate ? quizEndDate: "";
+			quizTitleString << quizTitle ? quizTitle : "";
+			quizDurationString << quizDuration ? quizDuration : "";
+			quizPoolString << quizPool ? quizPool : "";
+			quizStartDateString << quizStartDate ? quizStartDate : "";
+			quizEndDateString << quizEndDate ? quizEndDate : "";
 
-			if((quizTitleString.str() != "" && quizDurationString.str() != "" &&
-			quizPoolString.str() != "" && quizStartDateString.str() != "" && quizEndDateString.str() != "")) {
+			if ((quizTitleString.str() != "" && quizDurationString.str() != "" &&
+				quizPoolString.str() != "" && quizStartDateString.str() != "" && quizEndDateString.str() != "")) {
 				Quiz currentQuiz(quizTitleString.str(), quizStartDateString.str(), quizEndDateString.str(), stoi(quizDurationString.str()));
 				currentQuiz.saveQuiz();
 				std::cout << currentQuiz.getTitle() << "-------------" << std::endl; // for testing
-			}	
+			}
 		}
-		else if (filename == "savepool") {
-			QuestionPool q("pool1");
-			std::cout << q.addQuestion("Question 1", 1);
-			std::cout << q.addOption("Question 1", "Option 1", 1);
-			std::cout << q.addOption("Question 1", "Option 2", 0);
-			std::cout << q.addQuestion("Question 2", 1);
-			std::cout << q.addOption("Question 2", "Option 1", 0);
-			std::cout << q.addOption("Question 2", "Option 2", 1);
-			q.save();
-			std::string str = "createPool.html";
-			filename = str;
-	
-		}
+
 		sendHtml(res, filename);
-	});
+			});
 
 	CROW_ROUTE(app, "/scripts/<string>")
-	([](const request& req, response& res, string filename) {
+		([](const request& req, response& res, string filename) {
 		sendScript(res, filename);
-	});
+			});
 
 	CROW_ROUTE(app, "/styles/<string>")
-	([](const request& req, response& res, string filename) {
+		([](const request& req, response& res, string filename) {
 		sendStyle(res, filename);
-	});
+			});
 
 	// Images in parent images folder
 	CROW_ROUTE(app, "/images/<string>")
-	([](const request& req, response& res, string filename) {
+		([](const request& req, response& res, string filename) {
 		sendImage(res, filename);
-	});
+			});
 
 	CROW_ROUTE(app, "/quizzes/<string>")
-	([](const request& req, response& res, string filename) {
+		([](const request& req, response& res, string filename) {
 		sendQuizText(res, filename);
-	});
+			});
 	//serice port
 	app.port(27501).multithreaded().run();
 	return 1;
--- Enterprise_Solution_Quizzes/QuizSharedDocker/public/scripts/questionPoolForm.js
+++ Enterprise_Solution_Quizzes/QuizSharedDocker/public/scripts/questionPoolForm.js
@@ -1,73 +1,105 @@
 var formID = 0;
 var buttonID = 0;
 var optionID = 0;
+
+function start() {
+    const urlParams = new URLSearchParams(location.search);
+    for (const [key, value] of urlParams) {
+        if (key == "created" && value == "true") {
+            alert("Quiz Created");
+        }
+        else if (key == "created" && value == "false") {
+            alert("Quiz not Created");
+        }
+    }
+}
 function AddQuestion(i) {
     var t = String(i);
     var b = document.createElement('input');
     var d = document.createElement('input');
-var z = document.createElement('br');
+    var z = document.createElement('br');
     d.setAttribute("type", "text");
-    d.setAttribute("value", "question"+formID);
-    d.setAttribute("id", "Q"+formID);
+    d.setAttribute("value", "question" + formID);
+    d.setAttribute("id", "Q" + formID);
     b.setAttribute("type", "button");
     b.setAttribute("id", buttonID);
     b.setAttribute("value", "Add option");
-    b.setAttribute("onclick", "option(" + buttonID + ","+t+")");
+    b.setAttribute("onclick", "option(" + buttonID + "," + t + ")");
     buttonID++;
     var i = document.createElement('form');
     i.setAttribute("id", formID);
     formID++;
     i.appendChild(d);
     i.appendChild(b);
-    document.getElementById("insertHere").insertAdjacentElement("afterend",i);
- document.getElementById("insertHere").insertAdjacentElement("afterend",z);
- document.getElementById((formID-1) + "Q").insertAdjacentElement("beforebegin",z);
+    document.getElementById("insertHere").insertAdjacentElement("afterend", i);
+    document.getElementById("insertHere").insertAdjacentElement("afterend", z);
+    document.getElementById((formID - 1) + "Q").insertAdjacentElement("beforebegin", z);
 
 }
 
 function Enter() {
-  var query = "?pool="+document.getElementById("pname").value+"&&";
-  var ids = document.querySelectorAll('[id]');
+    var query = "?pool=" + document.getElementById("pname").value + "&&";
+    var ids = document.querySelectorAll('[id]');
     Array.prototype.forEach.call(ids, function (el, i) {
         // "el" is your element
         if (el.id.toString().includes("Q")) {
-query+=el.id.toString();
-query+= "=";
-var f = document.getElementById(el.id).value
-query+=f;
-query+="&&";
-           
-        }
-	else if (el.id.toString().includes("-")) {
-var f = document.getElementById(el.id).checked
-var d = el.id.toString().split('-');
-if(f == true) { 
-query+=("Q"+d[0]+"A"+d[1]+"C")
-query+= "=";
-query+=f;
-query+="&&";
-}
-            
-        }
-else if (el.id.toString().includes(":")) {
-var f = document.getElementById(el.id).value
-var d = el.id.toString().split(':');
-	  query+= ("Q"+d[0]+"A"+d[1]);
-query+= "=";
-query+=f;
-query+="&&";	
-            
-        }
-       
+            query += "Questions[]=";
+            var f = document.getElementById(el.id).value
+            query += f;
+            query += "&";
 
+        }
+    });
+    query += "&";
+
+    var checked = false;
+    var tmp = 0;
+    Array.prototype.forEach.call(ids, function (el, i) {
+      
+       
+        if (el.id.toString().includes(":")) {
+          
+            var f = document.getElementById(el.id).value
+            var d = el.id.toString().split(':');
+            if (checked == false) {
+                tmp = d[0];
+                checked = true;
+            }
+            if (d[0] == tmp) {
+                query += ("Q" + d[0] + "A[]=");
+                query += f;
+                query += "&";
+            }
+            else {
+                query += ("&Q" + d[0] + "A[]=");
+                query += f;
+                query += "&";
+                tmp--;
+            }
+        }
+    });
+    query += "&";
+    Array.prototype.forEach.call(ids, function (el, i) {
+        if (el.id.toString().includes("-")) {
+            var f = document.getElementById(el.id).checked
+            var d = el.id.toString().split('-');
+            if (f == true) {
+                query += ("Q" + d[0] + "A" + d[1] + "C")
+                query += "=";
+                query += f;
+                query += "&&";
+            }
+        }
     });
 
-var tmp = query.slice(0,-2);
-var currentHost = window.location.host;
-window.location.replace('/savepool'+tmp);
+
+
+    var tmp = query.slice(0, -2);
+    var currentHost = window.location.host;
+    window.location.replace('/savepool' + tmp);
 }
 function option(i, c) {
-var n = document.createElement('br');
+    var n = document.createElement('br');
     var b = String(i);
     var i = document.createElement('input');
     var z = document.createElement('input');
@@ -75,14 +107,14 @@ var n = document.createElement('br');
     i.setAttribute("name", b);
     z.setAttribute("type", "text");
     z.setAttribute("value", "answer");
-z.setAttribute("id", b + ":" + optionID);
+    z.setAttribute("id", b + ":" + optionID);
     optionID++;
     if (c == 0) {
         i.setAttribute("type", "radio")
-       
+
         document.getElementById(b).insertAdjacentElement("afterend", i);
         document.getElementById(b).insertAdjacentElement("afterend", z);
- document.getElementById(b).insertAdjacentElement("afterend", n);
+        document.getElementById(b).insertAdjacentElement("afterend", n);
     }
     else if (c == 1) {
         i.setAttribute("type", "checkbox")
@@ -92,5 +124,5 @@ z.setAttribute("id", b + ":" + optionID);
     else {
         alert("Unexpected input");
     }
-    
+
 }
