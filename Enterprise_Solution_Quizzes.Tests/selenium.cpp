6fb40f8a4b1c7acd2c4f44c0079dfd64a6df4ac9
Danny Smith <Dsmith2304@conestogac.on.ca>
2022-10-30 23:36:34 -0400

    Selenium testing done through chrome extension for question pool

---
+++ Enterprise_Solution_Quizzes.Tests/selenium.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+#include "Answer.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace EnterpriseSolutionQuizzesTests
+{
+    TEST_CLASS(Selenium)
+    {
+    public:
+        TEST_METHOD(seleniumStart)
+        {
+            
+          
+        }
+    }
+}
