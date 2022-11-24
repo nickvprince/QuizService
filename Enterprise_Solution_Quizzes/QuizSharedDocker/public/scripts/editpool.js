0edac0e388d445fb350921dfa37812da7b3545ac
Danny Smith <Dsmith2304@conestogac.on.ca>
2022-11-24 17:49:00 -0500

    Pushing merge results on main -CONFIRMED BY : Nick and Islam

--- Enterprise_Solution_Quizzes/QuizSharedDocker/public/scripts/editpool.js
+++ Enterprise_Solution_Quizzes/QuizSharedDocker/public/scripts/editpool.js
@@ -27,54 +27,65 @@ function load() {
 
     // Parse the services json file
     const json = xmlHttp.responseText;
+
     const obj = JSON.parse(json);
+    var counter = 0;
+    for (let x in obj) {
+        counter++;
+    }
+    console.log(counter)
+    
+    for (var i = counter; i > -1; i--) {
+        var countTo = 0;
+        for (let x in obj) { // add questions
+            if (countTo == i) {
+                console.log(x);
 
-    console.log(obj);
-    for (let x in obj) { // add questions
-        console.log(x);
-       
-        var b = x.toString();
-        var counter = 0;
-        var type = 0;
+                var b = x.toString();
+                var counter = 0;
+                var type = 0;
 
-        for (let k in obj[b]) { // get number of correct answers
-            // console.log(obj[b][k]);
-            if (obj[b][k] == "true") {
-                type++;
-            }
-        }
-        if (type > 1) { // if multiple choice or multiple answer
-            var ques = AddQuestion(1, String(x));
-        } else {
-            var ques = AddQuestion(0, String(x));
-        }
-        type = 0;
+                for (let k in obj[b]) { // get number of correct answers
+                    // console.log(obj[b][k]);
+                    if (obj[b][k] == "true") {
+                        type++;
+                    }
+                }
+                if (type > 1) { // if multiple choice or multiple answer
+                    var ques = AddQuestion(1, String(x));
+                } else {
+                    var ques = AddQuestion(0, String(x));
+                }
+                type = 0;
 
-        for (let n in obj[b]) { // add options
-            
-            console.log("Option : ");
-            for (let k in obj[b]) {
-               // console.log(obj[b][k]);
-                if (obj[b][k] == "true") {
-                    type++;
+                for (let n in obj[b]) { // add options
+
+                    console.log("Option : ");
+                    for (let k in obj[b]) {
+                        // console.log(obj[b][k]);
+                        if (obj[b][k] == "true") {
+                            type++;
+                        }
+                    }
+                    if (type > 1) {
+                        option(ques, 1, String(n));
+                    } else {
+                        option(ques, 0, String(n));
+                    }
+                    type = 0;
+                    if (obj[b][n] == "true") {
+                        document.getElementById(String(ques) + "-" + counter).checked = true;
+                    } else {
+                        document.getElementById(String(ques) + "-" + counter).checked = false;
+                    }
+                    counter++;
+
                 }
             }
-            if (type > 1) {
-                option(ques, 1, String(n));
-            } else {
-                option(ques, 0, String(n));
-            }
-            type = 0;
-            if (obj[b][n] == "true") {
-                document.getElementById(String(ques) + "-" + counter).checked = true;
-            } else {
-                document.getElementById(String(ques) + "-" + counter).checked = false;
-            }
-            counter++;
-           
+            countTo++;
         }
-
     }
+
 
 }
 
