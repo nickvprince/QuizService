301dae8410093817b27af5c27b4f03940bfa9f72
Babywise <islamdoesart@gmail.com>
2022-11-24 17:10:22 -0500

    Added getMode initializing files

---
+++ Enterprise_Solution_Quizzes/QuizSharedDocker/public/scripts/getMode.js
@@ -0,0 +1,16 @@
+function getMode(){
+    //get request mode
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", "", false);
+    xmlHttp.send(null);
+
+
+
+    var professor = true;
+
+    if(professor) {
+        //get request professorIndex.html
+    } else {
+        //get request studentIndex.html
+    }
+}
