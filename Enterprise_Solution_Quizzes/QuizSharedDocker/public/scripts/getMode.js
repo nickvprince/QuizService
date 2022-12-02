fd10bd678b6ac10935f2bb299d79f9204e8e1cd5
Danny Smith <Dsmith2304@conestogac.on.ca>
2022-12-02 13:00:07 -0500

    switch mode buttons now available on index page if admin is not available

--- Enterprise_Solution_Quizzes/QuizSharedDocker/public/scripts/getMode.js
+++ Enterprise_Solution_Quizzes/QuizSharedDocker/public/scripts/getMode.js
@@ -48,24 +48,53 @@ function getExecutionMode(){
         xmlHttpSetMode.open("GET", '/setMode/na', false);
         xmlHttpSetMode.send(null);
 
-        xmlHttpLog.open("POST", '../log/Professor mode defaulted... admin is not online at '+ip+':'+port+'/2', false);
+        xmlHttpLog.open("POST", '../log/admin%20is%20not%20online%20at '+ip+':'+port+'/2', false);
         xmlHttpLog.send(null);
 
-        professor = true;
+        addButtons();
     }
 
-    if(professor == false) {
-        if(professorObj["modeofoperation"] == true) {
-            redirectToAddr("../index.html");
-        } else {
-            redirectToAddr("../studentIndex.html");
-        }
-    } else {
-        redirectToAddr("../index.html");
-    }
+ 
 }
     
-function redirectToAddr (addr) {
+function redirectToAddr(addr) {
+   
     window.location.replace(addr);
 }
 
+function addButtons() {
+    
+    //init buttons
+    var buttonProf = document.createElement("input");
+    var buttonStudent = document.createElement("input");
+
+    // professor button
+   buttonProf.setAttribute("onclick", "alterAddr('./index.html')");
+    buttonProf.setAttribute("value", "professor");
+    buttonProf.setAttribute("type", "button");
+
+    //Student button
+    buttonStudent.setAttribute("onclick", "alterAddr('./studentIndex.html')");
+    buttonStudent.setAttribute("value", "student");
+    buttonStudent.setAttribute("type", "button");
+
+    // add buttons to page
+    document.getElementById("insertHere").insertAdjacentElement("afterend", buttonProf);
+    document.getElementById("insertHere").insertAdjacentElement("afterend", buttonStudent);
+}
+
+function alterAddr(addr) {
+
+    var xmlHttpSetMode = new XMLHttpRequest(); // used to set mode in server
+    if (addr == './index.html') {
+        xmlHttpSetMode.open("GET", '/setMode/prof', false);
+        xmlHttpSetMode.send(null);
+ 
+    }
+    else {
+        xmlHttpSetMode.open("GET", '/setMode/stud', false);
+        xmlHttpSetMode.send(null);
+       
+    }
+    window.location.replace(addr);
+}
